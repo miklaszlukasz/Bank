15c0b7c3972afe43a9ec39fb455946ffeed2ff81
Lukasz Miklasz <miklaszlukasz@gmail.com>
2016-07-21 12:55:48 +0200

    dodanie daty urodzenia do usera

--- src/main/java/Account/User.java
+++ src/main/java/Account/User.java
@@ -1,15 +1,33 @@
 package Account;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class User {
 	private String firstName;
 	private String lastName;
 	private String idNumber;
+	private Date dateOfBirth;
 	
 	public User(String firstName, String lastName, String idNumber) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
+		dateOfBirth = generateDateOfBirthFromIdNumber();
+	}
+	
+	private Date generateDateOfBirthFromIdNumber() {
+		int yearOfBirth = Integer.valueOf(idNumber.substring(0, 2)) + 1900;
+		int monthOfBirth = Integer.valueOf(idNumber.substring(2, 4));
+		int dayOfBirth = Integer.valueOf(idNumber.substring(4, 6));
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			return formatter.parse(dayOfBirth + "/" + monthOfBirth + "/" + yearOfBirth);
+		} catch (ParseException e) {
+			return null;
+		}
 	}
 	
 	public String getFirstName() {
@@ -22,5 +40,9 @@ public class User {
 
 	public String getIdNumber() {
 		return idNumber;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
 	}
 }
--- src/test/java/Account/UserTest.java
+++ src/test/java/Account/UserTest.java
@@ -2,12 +2,23 @@ package Account;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 
 public class UserTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUser() throws ParseException {
+		String firstName= "Jan";
+		String lastName= "Kowalski";
+		String idNumber= "76041019253";
+		User user= new User(firstName, lastName, idNumber);
+		assertEquals(firstName, user.getFirstName());
+		assertEquals(lastName, user.getLastName());
+		assertEquals(idNumber, user.getIdNumber());
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		assertEquals(formatter.parse("10/04/1976"), user.getDateOfBirth());
 	}
 }
