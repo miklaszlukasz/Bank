148adcf4e374cf697d24a839e2745afd4e2581b3
Lukasz Miklasz <miklaszlukasz@gmail.com>
2016-07-21 15:20:24 +0200

    Add new classes Transaction, Account and tests to this, now all are empty

---
+++ src/main/java/Account/Account.java
@@ -0,0 +1,8 @@
+package Account;
+
+import java.util.List;
+
+public class Account {
+	private double money;
+	private List <Transaction> transactionHistory;
+}
---
+++ src/test/java/Account/AccountTest.java
@@ -0,0 +1,14 @@
+package Account;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
