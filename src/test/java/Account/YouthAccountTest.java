b778871d876ff3dd14c39ec619a79dcd3c560781
Lukasz Miklasz <miklaszlukasz@gmail.com>
2016-07-29 07:45:00 +0200

    change algorythm generate day of birth, change tests in youthaccount

--- src/test/java/Account/YouthAccountTest.java
+++ src/test/java/Account/YouthAccountTest.java
@@ -1,62 +1,60 @@
 package Account;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import Transaction.Transaction;
 import Transaction.TransactionType;
 import Transaction.Transfer;
+import junit.framework.TestCase;
 
-public class YouthAccountTest {
+public class YouthAccountTest extends TestCase {
 	private final double epsilon = 0.0;
 	private User owner;
 	private User supervisor;
-	private Account personalAccount;
+	private Account youthAccount;
 	private double moneyOnAccount;
 	private Transaction transcaction;
 
 	public void setUp() {
 		owner = new User.Builder().build();
-		personalAccount = new YouthAccount(owner, supervisor);
-		assertEquals(moneyOnAccount, personalAccount.getMoney(), epsilon);
+		youthAccount = new YouthAccount(owner, supervisor);
+		assertEquals(moneyOnAccount, youthAccount.getMoney(), epsilon);
 	}
 	
 	@Test
-	public void testDeposit() {
+	public void testDepositOnOwner() {
 		double moneyToDeposit = 1000.63;
 		moneyOnAccount += moneyToDeposit;
-		personalAccount.depositMoney(moneyToDeposit);
-		assertEquals(moneyOnAccount, personalAccount.getMoney(), epsilon);
-		assertEquals(1, personalAccount.getTransactionsHistory().size());
+		youthAccount.depositMoney(moneyToDeposit);
+		assertEquals(moneyOnAccount, youthAccount.getMoney(), epsilon);
+		assertEquals(1, youthAccount.getTransactionsHistory().size());
 		
-		transcaction = personalAccount.getTransactionsHistory().get(0);
+		transcaction = youthAccount.getTransactionsHistory().get(0);
 		assertEquals(TransactionType.DEPOSIT, transcaction.getType());
-		assertEquals(personalAccount, transcaction.getPerformer());
+		assertEquals(youthAccount, transcaction.getPerformer());
 		assertEquals(moneyToDeposit, transcaction.getAmount(), epsilon);
-		
 	}
 	
 	@Test
-	public void testWithdraw() {
+	public void testWithdrawOnOwner() {
 		double monetToWithdraw = 666.66;
 		moneyOnAccount -= monetToWithdraw;
-		personalAccount.withdrawMoney(monetToWithdraw);
-		assertEquals(moneyOnAccount, personalAccount.getMoney(), epsilon);
-		assertEquals(1, personalAccount.getTransactionsHistory().size());
+		youthAccount.withdrawMoney(monetToWithdraw);
+		assertEquals(moneyOnAccount, youthAccount.getMoney(), epsilon);
+		assertEquals(1, youthAccount.getTransactionsHistory().size());
 		
-		transcaction = personalAccount.getTransactionsHistory().get(0);
+		transcaction = youthAccount.getTransactionsHistory().get(0);
 		assertEquals(TransactionType.WITHDRAW, transcaction.getType());
-		assertEquals(personalAccount, transcaction.getPerformer());
+		assertEquals(youthAccount, transcaction.getPerformer());
 		assertEquals(monetToWithdraw, transcaction.getAmount(), epsilon);
 	}
 	
 	@Test
-	public void testTransfer() {
+	public void testTransferOnOwner() {
 		double moneyToTransfer = 333.33;
 
 		moneyOnAccount = 452.73;
-		personalAccount.depositMoney(moneyOnAccount);
+		youthAccount.depositMoney(moneyOnAccount);
 		moneyOnAccount -= moneyToTransfer;
 
 		Account recipient = new PersonalAccount(owner);
@@ -64,22 +62,36 @@ public class YouthAccountTest {
 		recipient.depositMoney(moneyOnRecipient);
 		moneyOnRecipient += moneyToTransfer;
 		
-		personalAccount.transferMoney(recipient, moneyToTransfer, "test");
-		assertEquals(moneyOnAccount, personalAccount.getMoney(), epsilon);
+		youthAccount.transferMoney(recipient, moneyToTransfer, "test");
+		assertEquals(moneyOnAccount, youthAccount.getMoney(), epsilon);
 		assertEquals(moneyOnRecipient, recipient.getMoney(), epsilon);
 		
-		Transfer transfer = (Transfer) personalAccount.getTransactionsHistory().get(1);
+		Transfer transfer = (Transfer) youthAccount.getTransactionsHistory().get(1);
 		assertEquals(TransactionType.TRANSFER, transfer.getType());
-		assertEquals(personalAccount, transfer.getPerformer());
+		assertEquals(youthAccount, transfer.getPerformer());
 		assertEquals(moneyToTransfer, transfer.getAmount(), epsilon);
 		assertEquals(recipient, transfer.getRecipient());
 		
 		Transfer recipientTranscaction = (Transfer) recipient.getTransactionsHistory().get(1);
 		assertEquals(TransactionType.TRANSFER, recipientTranscaction.getType());
-		assertEquals(personalAccount, recipientTranscaction.getPerformer());
+		assertEquals(youthAccount, recipientTranscaction.getPerformer());
 		assertEquals(moneyToTransfer, recipientTranscaction.getAmount(), epsilon);
 		assertEquals(recipient, recipientTranscaction.getRecipient());
 		
 		assertEquals(transfer, recipientTranscaction);
+	}
+	
+	@Test
+	public void testDepositOnSupervisor() {
+		double moneyToDeposit = 1000.63;
+		moneyOnAccount += moneyToDeposit;
+		youthAccount.depositMoney(moneyToDeposit);
+		assertEquals(moneyOnAccount, youthAccount.getMoney(), epsilon);
+		assertEquals(1, youthAccount.getTransactionsHistory().size());
+		
+		transcaction = youthAccount.getTransactionsHistory().get(0);
+		assertEquals(TransactionType.DEPOSIT, transcaction.getType());
+		assertEquals(youthAccount, transcaction.getPerformer());
+		assertEquals(moneyToDeposit, transcaction.getAmount(), epsilon);
 	}
 }
