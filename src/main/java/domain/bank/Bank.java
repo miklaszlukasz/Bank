d7fd7496669692ad61576ff10629ad453d8e3876
Lukasz Miklasz <miklaszlukasz@gmail.com>
2016-08-10 14:48:48 +0200

    remove rubish from Bank

--- src/main/java/domain/bank/Bank.java
+++ src/main/java/domain/bank/Bank.java
@@ -26,16 +26,4 @@ public class Bank implements UsersAccountsControlable {
 				return userAccount;
 		return null;
 	}
-	// Scanner scanner = new Scanner(System.in);
-	// System.out.println("Input your id number:");
-	// final String idNumber = scanner.nextLine();
-	// System.out.println("Input password:");
-	// final String password = scanner.nextLine();
-	// System.out.println("Input your first name: ");
-	// final String firstName = scanner.nextLine();
-	// System.out.println("Input your last name: ");
-	// final String lastName = scanner.nextLine();
-	// scanner.close();
-	// return new UserAccount.Builder(idNumber,
-	// password).firstName(firstName).lastName(lastName).generateDateOfBirthFromIdNumber().build();
 }
